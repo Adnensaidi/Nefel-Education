6c3441ba231f205eae19b4b744ce4acb63fd49a2
Adnen Saidi <adnensaidi8294@gmail.com>
2024-12-12 11:13:34 +0100

    Predict Arrays

---
+++ Programing Basics/week1/day2/core/Predict Arrays/predict_arrays.js
@@ -0,0 +1,28 @@
+// Code Snippet 1
+
+
+var contactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+console.log(contactInfo);
+
+// output : ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345]
+
+
+// Code Snippet 2
+
+
+var produce = ["apples", "oranges"];
+var frozen = ["broccoli", "ice cream"];
+frozen.push("hashbrowns");
+console.log(frozen);
+
+// output : ["broccoli", "ice cream"]
+
+// Code Snippet 3
+
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zorro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+// output : ["Bambi", "Beetlejuice", "Toy Story", "Zorro"]
