b7e9d11e31b0ce5a1212c95b1099a30691ec62c9
Adnen Saidi <adnensaidi8294@gmail.com>
2024-12-24 13:12:25 +0100

    Nefel_Education

---
+++ Web_Fundamentals/Week_2/Day_1/Practice/Video_Preview/script.js
@@ -0,0 +1,16 @@
+
+function playVideo(vid) {
+    vid.play();
+}
+
+function pauseVideo(vid) {
+    vid.pause();
+    vid.currentTime = 0;
+}
+    // function over(element) {
+    //     alert("mouseover");    
+    // }
+        
+    // function out(element) {
+    //     alert("mouseout");    
+    // }
