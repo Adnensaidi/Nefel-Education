5d60a491531dd9dfad8828496999a012b00b4bea
Adnen <adnensaidi8294@gmail.com>
2025-01-14 15:26:01 +0100

    Nefel Education

---
+++ Python/MySQL/Week_2/Day_2/Practice/MySQL Workbench Setup/names.sql
@@ -0,0 +1,6 @@
+SELECT * FROM names.names;
+INSERT INTO names (name) VALUES ('Adnen');
+INSERT INTO names (name) VALUES ('Ali'), ('Taher'), ('Massoud');
+UPDATE names SET name = 'Jasser' WHERE id = 1;
+DELETE FROM names WHERE id = 1;
+
