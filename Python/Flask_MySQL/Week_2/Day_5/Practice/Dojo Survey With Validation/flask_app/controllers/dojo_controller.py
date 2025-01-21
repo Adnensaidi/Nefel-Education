ebd03b438b88722309b5ae2e1b5a91ed1fc23a8b
Adnen <adnensaidi8294@gmail.com>
2025-01-21 07:52:38 +0100

    Nefel Education

---
+++ Python/Flask_MySQL/Week_2/Day_5/Practice/Dojo Survey With Validation/flask_app/controllers/dojo_controller.py
@@ -0,0 +1,21 @@
+from flask import render_template,request, redirect
+from flask_app import app
+from flask_app.models.dojo_model import Dojo
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/create/dojo',methods=['POST'])
+def create_dojo():
+    if Dojo.is_valid(request.form):
+        Dojo.save(request.form)
+        return redirect('/results')
+    return redirect('/')
+
+@app.route('/results')
+def results():
+    last_dojo = Dojo.get_last_dojos()
+    return render_template('results.html', dojo=last_dojo)
