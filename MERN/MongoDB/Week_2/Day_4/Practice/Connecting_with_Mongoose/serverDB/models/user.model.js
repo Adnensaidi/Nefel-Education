636cfdbb3ce998633a3a10bde6655add6825b876
Adnen <adnensaidi8294@gmail.com>
2025-02-25 15:38:22 +0100

    Nefel Education

---
+++ MERN/MongoDB/Week_2/Day_4/Practice/Connecting_with_Mongoose/serverDB/models/user.model.js
@@ -0,0 +1,15 @@
+// import mongoose from 'mongoose'
+
+// const userSchema= new mongoose.Schema({ 
+// 	firstName:{
+// 		Type:String,
+// 		Required:true,
+// 		},
+// 	email:{
+// 	type: string,
+// 	unique:true
+// 	}
+// })
+// const User= mongoose.model('user',userSchema)
+
+// export default User
