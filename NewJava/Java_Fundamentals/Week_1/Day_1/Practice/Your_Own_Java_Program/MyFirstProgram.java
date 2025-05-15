fec166a1788fc96caf68f5bf6886ec2ff401a56a
Adnen <adnensaidi8294@gmail.com>
2025-05-15 14:39:09 +0100

    Nefel Education

---
+++ NewJava/Java_Fundamentals/Week_1/Day_1/Practice/Your_Own_Java_Program/MyFirstProgram.java
@@ -0,0 +1,7 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Adnen Saidi");
+        System.out.println("I am 31 years old");
+        System.out.println("My hometown is Tunisia ,Marsa");
+    }
+}
